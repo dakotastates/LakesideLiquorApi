93e7236fce2b64aebee47ac8909bc5f101d54790
Dakota States <dakota.states@gmail.com>
2021-01-08 15:40:02 -0800

    user and recipe model

---
+++ db/migrate/20210108233226_create_recipes.rb
@@ -0,0 +1,15 @@
+class CreateRecipes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :author
+      t.text :description
+      t.string :image_url
+      t.text :instructions
+      t.string :source
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
